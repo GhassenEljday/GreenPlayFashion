e1888f82a69683a36dd51e8baee8c8d3f4e7e7bf
GhassenEljday <ghassenjday2001@gmail.com>
2020-07-14 00:06:53 +0100

    don

--- GPF2.js
+++ GPF2.js
@@ -4,3 +4,14 @@
  		$("#alert2").click(function(){
  	alert("You can't sign up for now" );
  });
+
+ 	function send () {
+	var name = $("#esem").val();
+	var msg = $("#ekteb").val();
+
+	if (name !== "string" || name === undefined) {
+		alert( "Write a clear name " )
+	}
+	$("#ab").click(function(){
+		send();
+	})
